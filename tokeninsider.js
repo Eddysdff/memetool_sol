79d02878817181b665316219e628b000064e1209
Eddysdff <100467007@qq.com>
2025-03-03 21:02:37 +0800

    钱包关联及池子锁定

---
+++ tokeninsider.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+const tokenReportUrl = 'https://api.rugcheck.xyz/v1/tokens/Bd5oLZJHiUkwmBtSYuEJmeR4LiQif8Tdcwg61myzXF1R/insiders/graph';
+
+axios.get(tokenReportUrl, {
+  headers: {
+    'accept': 'application/json'
+  }
+})
+.then(response => {
+  console.log('完整返回数据:', JSON.stringify(response.data, null, 2));
+})
+.catch(error => {
+  console.error('请求错误:', error);
+});
